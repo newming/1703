c8371e1a9f5cf1bd469b5e4215ec1f3cc259de5b
newming <a977527479@sina.com>
2017-06-26 11:46:47 +0800

    modal

--- react-hello/src/App.js
+++ react-hello/src/App.js
@@ -3,10 +3,38 @@ import React from 'react'
 import Modal from './Modal'
 
 class App extends React.Component{
+	constructor(){
+		super()
+		this.state = {
+			signin: false,
+			signup: false
+		}
+	}
+	handleLogin(){
+		alert('你要登录了')
+		this.setState({signin: false})
+	}
 	render(){
 		return(
 			<div>
-				<Modal />
+				<button onClick={()=>this.setState({signin: true})}>登录</button>
+				<button onClick={()=>this.setState({signup: true})}>注册</button>
+
+				<Modal visible={this.state.signin} title='登录' onOk={this.handleLogin.bind(this)} onCancle={()=>this.setState({signin: false})}>
+					<label htmlFor="username">用户名：</label>
+					<input type="text" name='username'/>
+					<label htmlFor="password">密码：</label>
+					<input type="text" name='password'/>
+				</Modal>
+
+				<Modal visible={this.state.signup} title='注册'>
+					<label htmlFor="username">用户名：</label>
+					<input type="text" name='username'/>
+					<label htmlFor="password">密码：</label>
+					<input type="text" name='password'/>
+					<label htmlFor="confirmPassword">确认密码：</label>
+					<input type="text" name='confirmPassword'/>
+				</Modal>
 			</div>
 		)
 	}
--- react-hello/src/Modal.js
+++ react-hello/src/Modal.js
@@ -2,11 +2,15 @@ import React from 'react'
 import './main.css'
 
 class Modal extends React.Component{
-	constructor(){
-		super()
+	constructor(props){
+		super(props)
 		this.state={
-			visible: true
+			visible: props.visible
 		}
+	}
+	componentWillReceiveProps(nextProps){
+		// console.log(nextProps)
+		this.setState({visible: nextProps.visible})
 	}
 	render(){
 		let styles = {
@@ -14,25 +18,32 @@ class Modal extends React.Component{
 				display: this.state.visible?'block':'none'
 			}
 		}
+		let {title, cancelText, okText, children, onOk, onCancle} = this.props;
 		return(
 			<div className='modal-wrap' style={styles.root}>
 				<div className='modal-card'>
 					<div className='modal-header'>
-						<h3>title</h3>
-						<span>关闭</span>	
+						<h3>{title}</h3>
+						<span onClick={()=>this.setState({visible: false})}>关闭</span>
 					</div>
-					<div className='modal-body'>
-						<p>some contents</p>
-						<p>some contents</p>
-						<p>some contents</p>
-					</div>
+
+					<div className='modal-body'>{children}</div>
+
 					<div className='modal-control'>
-						<button>Cancle</button>
-						<button>OK</button>
+						<button onClick={()=>{onCancle && onCancle()}}>{cancelText}</button>
+						<button onClick={()=>{onOk && onOk()}}>{okText}</button>
 					</div>
 				</div>
 			</div>
 		)
 	}
 }
+
+Modal.defaultProps = {
+	title: 'default title',
+	cancelText: 'Cancle',
+	okText: 'OK',
+	children: ''
+}
+
 export default Modal
