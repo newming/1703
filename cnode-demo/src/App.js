37cd729e67a0a36e60388789824fb75b21ffae47
newming <a977527479@sina.com>
2017-07-03 09:23:53 +0800

    router

--- cnode-demo/src/App.js
+++ cnode-demo/src/App.js
@@ -1,40 +1,21 @@
 import React from 'react'
-import axios from 'axios'
-import ShowTopics from './component/ShowTopics'
+import Home from './component/Home'
+import {BrowserRouter, Route} from 'react-router-dom'
 
 class App extends React.Component{
-	constructor(){
-		super()
-		this.state = {
-			data: [],
-			tabs: ['all', 'share', 'ask', 'job', 'good'],
-			tab: 'all'
-		}
-	}
-	componentDidMount(){
-		axios.get('https://cnodejs.org/api/v1/topics')
-			.then( res => this.setState({data: res.data.data}) )
-			.catch( err => alert(err) )
-	}
-	handleClick(tab){
-		// console.log(tab)
-		this.setState({tab: tab})
-		if (tab==='all') {tab=''}; 
-		axios.get('https://cnodejs.org/api/v1/topics?tab='+tab)
-			.then( res => this.setState({data: res.data.data}) )
-			.catch( err => alert(err) )
-	}
+	
 	render(){
-		console.log(this.state.data)
-		let {data, tabs, tab} = this.state;
-		let tabsSort = {all: '全部', ask: '问答', share: '分享', job: '工作', good: '精华'}
+		
 		return(
-			<div>
-				{
-					tabs.map( (item, i) => <button style={{background: tab===item ? 'green' : '#eee'}} key={i} onClick={this.handleClick.bind(this, item)}>{tabsSort[item]}</button>)
-				}
-				<ShowTopics data={data} />
-			</div>
+			<BrowserRouter>
+				<div>
+					<h1>cnode header</h1>
+
+					<Route path='/' exact component={Home} />
+					
+					<h1>cnode footer</h1>
+				</div>
+			</BrowserRouter>
 		)
 	}
 }
